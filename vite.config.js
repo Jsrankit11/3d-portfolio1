5d240def33f1f6fa7d742f9252704bb1deae2fdc
Ankit Chaudhary <noreply@github.com>
2026-03-24 00:42:21 +0530

    fix: update base path for 3d-portfolio1 deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/3d-portfolio/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/3d-portfolio1/" : "/",
   plugins: [react(), tailwindcss()],
 });
